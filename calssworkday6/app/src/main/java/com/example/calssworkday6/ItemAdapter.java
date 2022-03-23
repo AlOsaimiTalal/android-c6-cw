5f6f693ad2d5356ad456aa6b161481dbb24ddaa8
AlOsaimiTalal <100706870+AlOsaimiTalal@users.noreply.github.com>
2022-03-23 16:35:25 +0300

    done

---
+++ calssworkday6/app/src/main/java/com/example/calssworkday6/ItemAdapter.java
@@ -0,0 +1,45 @@
+package com.example.calssworkday6;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class ItemAdapter extends ArrayAdapter<Items> {
+    List<Items> item;
+    public ItemAdapter(@NonNull Context context, int resource, @NonNull List<Items> objects) {
+        super(context, resource, objects);
+        item = objects;
+
+
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.item_list,parent,false);
+
+        Items currentItems = item.get(position);
+        TextView itemName = view.findViewById(R.id.itemName);
+        TextView itemPrice = view.findViewById(R.id.itemPrice);
+        ImageView itemImg = view.findViewById(R.id.itemImage);
+
+        itemName.setText(currentItems.getItemName());
+        itemImg.setImageResource(currentItems.getItemImage());
+
+        return view;
+
+
+
+
+
+    }
+}
